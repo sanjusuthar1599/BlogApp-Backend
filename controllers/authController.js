b05c172014771e2b77cb03e50a3891a78f6761e8
Sanjay <Sanjusuthar1599@gmail.com>
2025-07-03 12:21:39 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,7 +67,9 @@ const getuser = async (req, res) => {
 //  * Verifies the user's email using a token.
 const sendEmailForVerify = async (userDetails) => {
   try {
-    const clientUrl = process.env.APP || "http://localhost:3000";
+    // const clientUrl = process.env.APP || "http://localhost:3000";
+const clientUrl = "https://blog-a-frontend.vercel.app";
+
     const verifyUrl = `${clientUrl}/api/auth/verify-email/?token=${userDetails.auth_token}`;
 
     const emailOptions = {
