f97b42fb11b077657b3633ff4dde72f0da6887b9
Sanjay <Sanjusuthar1599@gmail.com>
2025-07-03 16:55:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,17 +13,9 @@ const app = express();
 
 connectDB();
 
-// app.use(cors());
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://bloga-frontend.onrender.com"],
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use("/uploads", express.static("uploads"));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get("/", (req, res) => {
